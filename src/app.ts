0a1bddf5ffdcd238fbf13a92f080a235a4685ece
Lucas Santos <lhs.santoss@gmail.com>
2021-10-13 16:11:19 -0300

    add base app
    Signed-off-by: Lucas Santos <lhs.santoss@gmail.com>

--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,14 @@ import express from 'express'
 
 const app = express()
 
-app.get('/', (req, res) => {
-  res.send('pong')
+app.get('/render', (req, res) => {
+  const timeout = Math.floor(Math.random() * (1e6 - 1e5 + 1) + 1e5)
+  console.log(`Set timeout of ${timeout}`)
+  for (let i = 0; i < timeout; i++) {
+    console.log(`rendering ${i}`)
+  }
+
+  res.send(`Rendered in ${timeout}`)
 })
 
 app.listen(process.env.PORT || 3000, () => {
